e66a0bc6905b5f314d247eb26fe055b245ba16e7
Rafael Timbó <timbo.rafa@gmail.com>
2018-04-03 01:13:28 -0400

    afDatabase synchronized array fix

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  questions: AngularFireList<any>;
-  
-  constructor(public navCtrl: NavController, afDatabase: AngularFireDatabase) {
-    this.questions = afDatabase.list('/questions').valueChanges();
+  questions: Observable<any[]>
+  constructor(public navCtrl: NavController, private afDatabase: AngularFireDatabase) {
+    this.questions = this.afDatabase.list('questions').valueChanges()
   }
 
 }
