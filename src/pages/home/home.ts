9eb75c1ef10451ac9fc28fc38b4162d9680cd392
Rafael Timbó <timbo.rafa@gmail.com>
2018-04-02 21:27:23 -0400

    simple question and answers

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  questions: AngularFireList<any>;
+  
+  constructor(public navCtrl: NavController, afDatabase: AngularFireDatabase) {
+    this.questions = afDatabase.list('/questions').valueChanges();
   }
 
 }
