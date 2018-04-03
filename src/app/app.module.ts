480946bceff168884ff56cd05fad732c158820ac
Rafael Timbó <timbo.rafa@gmail.com>
2018-04-02 20:27:52 -0400

    Firebase Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+// <script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script> ???
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZItAzwCj8cDd0B1vF7pCaAykzBDSCVW4",
+  authDomain: "ionic-survey.firebaseapp.com",
+  databaseURL: "https://ionic-survey.firebaseio.com",
+  projectId: "ionic-survey",
+  storageBucket: "ionic-survey.appspot.com",
+  messagingSenderId: "277842474556"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
